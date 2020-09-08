65a1320c54df54cadc7fd22b0dcad2c42d16b71f
Alim Nastaev <a.nastaev@gmail.com>
2020-09-08 11:11:45 -0400

    format

--- sum-of-multiples/lib/sum_of_multiples.ex
+++ sum-of-multiples/lib/sum_of_multiples.ex
@@ -3,6 +3,7 @@ defmodule SumOfMultiples do
   Adds up all numbers from 1 to a given end number that are multiples of the factors provided.
   """
   def to(limit, factors), do: sum(limit, factors)
+
   defp sum(limit, factors) do
     for number <- 0..(limit - 1), parse(number, factors), reduce: 0 do
       acc -> number + acc
