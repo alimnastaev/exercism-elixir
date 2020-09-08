5544b485dd2eb5ca5bd03c9c27fdf24756d76346
Alim Nastaev <a.nastaev@gmail.com>
2020-09-08 11:03:51 -0400

    sum_of_multiples implemented

---
+++ sum-of-multiples/lib/sum_of_multiples.ex
@@ -0,0 +1,14 @@
+defmodule SumOfMultiples do
+  @doc """
+  Adds up all numbers from 1 to a given end number that are multiples of the factors provided.
+  """
+  def to(limit, factors), do: sum(limit, factors)
+  def sum(limit, factors) do
+    for number <- 0..(limit - 1), parse(number, factors), reduce: 0 do
+      acc -> number + acc
+    end
+  end
+
+  defp parse(number, factors),
+    do: Enum.any?(factors, fn factor -> rem(number, factor) == 0 end)
+end
