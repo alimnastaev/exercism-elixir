5c32dae47a32f8da37c365e5b800b6fb55a5d9fd
Alim Nastaev <a.nastaev@gmail.com>
2020-10-03 19:40:46 -0400

    beer_song refactored

--- beer-song/lib/beer_song.ex
+++ beer-song/lib/beer_song.ex
@@ -2,19 +2,19 @@ defmodule BeerSong do
   @doc """
   Get a single verse of the beer song
   """
-  def verse(number) when number == 2,
+  def verse(2),
     do: """
-    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    2 bottles of beer on the wall, 2 bottles of beer.
     Take one down and pass it around, 1 bottle of beer on the wall.
     """
 
-  def verse(number) when number == 1,
+  def verse(1),
     do: """
-    #{number} bottle of beer on the wall, #{number} bottle of beer.
+    1 bottle of beer on the wall, 1 bottle of beer.
     Take it down and pass it around, no more bottles of beer on the wall.
     """
 
-  def verse(number) when number == 0,
+  def verse(0),
     do: """
     No more bottles of beer on the wall, no more bottles of beer.
     Go to the store and buy some more, 99 bottles of beer on the wall.
@@ -30,6 +30,6 @@ defmodule BeerSong do
   Get the entire beer song for a given range of numbers of bottles.
   """
   def lyrics(range \\ 99..0) do
-    Enum.map_join(range, "\n", fn number -> verse(number) end)
+    Enum.map_join(range, "\n", &verse/1)
   end
 end
